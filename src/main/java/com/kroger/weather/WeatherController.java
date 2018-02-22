e758eebe7050c9357d180f0adb05a308835c41ff
Chris Nelson <chris@gaslight.co>
2018-02-22 11:02:29 -0500

    passing test

--- src/main/java/com/kroger/weather/WeatherController.java
+++ src/main/java/com/kroger/weather/WeatherController.java
@@ -1,5 +1,13 @@
 package com.kroger.weather;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WeatherController {
 
+    @RequestMapping(path = "/weather")
+    public String getWeather() {
+        return "It is sunny today";
+    }
 }
